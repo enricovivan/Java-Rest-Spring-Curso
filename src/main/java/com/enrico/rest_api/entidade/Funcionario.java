afc6de9485eeaf06b57781008993b71fbe126f6a
= <enricovivan21@gmail.com>
2021-10-16 15:47:21 -0300

    Definição de Entidade

---
+++ src/main/java/com/enrico/rest_api/entidade/Funcionario.java
@@ -0,0 +1,62 @@
+package com.enrico.rest_api.entidade;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Funcionario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String telefone;
+    @Column(nullable = false)
+    private String email;
+    private String url_avatar;
+    @Column(nullable = false)
+    private String funcao;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getUrl_avatar() {
+        return url_avatar;
+    }
+    public void setUrl_avatar(String url_avatar) {
+        this.url_avatar = url_avatar;
+    }
+    public String getFuncao() {
+        return funcao;
+    }
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    
+}
